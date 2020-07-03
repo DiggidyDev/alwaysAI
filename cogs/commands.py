c64145ca9860348600f1b86289082cb4eec8cb06
WiseNat <nathan88wise@gmail.com>
2020-07-03 13:41:54 +0100

    Discord Attachment -> OpenCV img

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,10 +1,21 @@
 from discord.ext import commands
+import numpy as np
+import cv2
 
 
 class Commands(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.is_owner()
+    @commands.command()
+    async def model(self, ctx):
+        img_bytes = await ctx.message.attachments[0].read()  # TODO Run the model for multiple images
+        nparr = np.fromstring(img_bytes, np.uint8)
+        img_np = cv2.imdecode(nparr, 1)
+
+
 
 
 def setup(bot):
