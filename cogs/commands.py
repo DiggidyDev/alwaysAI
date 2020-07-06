754e54e17d880d5c9116e2078303ca293d3ae410
WiseNat <nathan88wise@gmail.com>
2020-07-06 12:04:37 +0100

    Added: new model, confidence arg, comments

--- cogs/commands.py
+++ cogs/commands.py
@@ -116,31 +116,34 @@ class Commands(commands.Cog):
 
         await ctx.send(embed=embed)
 
-    @commands.is_owner()
+    # TODO Add in nicer error message if user doesn't define a model
+    # TODO Add in model list command
+    # TODO Install more models
+    # TODO Model install command - owner only
     @commands.command()
-    async def model(self, ctx):
+    async def model(self, ctx, model, confidence=0.5):  # Only functions for Object Detection
         # Getting image and converting it to appropriate data type
         img_bytes = await ctx.message.attachments[0].read()  # TODO Run the model for multiple images
         nparr = np.fromstring(img_bytes, np.uint8)
         img_np = cv2.imdecode(nparr, 1)
 
-        # Copy paste
-        obj_detect = edgeiq.ObjectDetection("alwaysai/res10_300x300_ssd_iter_140000")  # Todo use specified model
+        # AAI Magic
+        obj_detect = edgeiq.ObjectDetection(model)  # model example: "alwaysai/res10_300x300_ssd_iter_140000"
         obj_detect.load(engine=edgeiq.Engine.DNN)
         centroid_tracker = edgeiq.CentroidTracker(deregister_frames=100, max_distance=50)
 
-        results = obj_detect.detect_objects(img_np, confidence_level=.5)
+        results = obj_detect.detect_objects(img_np, confidence_level=confidence)
         objects = centroid_tracker.update(results.predictions)
 
         predictions = []
         for (object_id, prediction) in objects.items():
-            prediction.label = "face {}".format(object_id)
+            prediction.label = "Object {}".format(object_id)
             predictions.append(prediction)
 
         image = edgeiq.markup_image(img_np, predictions)
 
         # TODO Run the model for multiple images
-        # TODO Find a way to send bytes img to Discord
+        # Converting magic for Discord
         with Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)) as im:
             output_buffer = BytesIO()
             im.save(output_buffer, "png")
