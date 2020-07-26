1034fc01a6754e86a4b104b4d8ffbaadbf66c481
WiseNat <nathan88wise@gmail.com>
2020-07-26 16:34:21 +0100

    JSON Error Logging System

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
+import json
+import os
 import traceback
+from datetime import datetime
 
 import discord
 from discord.ext import commands
@@ -11,7 +14,38 @@ async def generate_user_error_embed(ctx, message):
 
 async def send_traceback(ctx, exception):
     tb_lines = traceback.format_exception(type(exception), exception, exception.__traceback__, 4)
-    await ctx.send("An unexpected error occurred ~\n```Python\n{}```".format(''.join(tb_lines)))
+    await ctx.send("An unexpected error occurred and will be logged ~\n```Python\n{}```".format(''.join(tb_lines)))
+
+    today = datetime.utcnow()
+    path_pattern = "logs/{}-{}-{} %s.json".format(today.day, today.month, today.year)
+    i = 1
+
+    # First do an exponential search
+    while os.path.exists(path_pattern % i):
+        i = i * 2
+
+    # Result lies somewhere in the interval (i/2..i]
+    # We call this interval (a..b] and narrow it down until a + 1 = b
+    a, b = (i // 2, i)
+    while a + 1 < b:
+        c = (a + b) // 2  # interval midpoint
+        a, b = (c, b) if os.path.exists(path_pattern % c) else (a, c)
+
+    path = path_pattern % b
+
+    # Saving log to JSON file - easier to manage later on if needed
+    with open(path, "w") as logfile:
+        command = ctx.invoked_with
+        args = [str(arg) for arg in ctx.args]
+        kwargs = [str(kwargs) for kwargs in ctx.kwargs]
+
+        data = {"User ID": ctx.author.id,
+                "Command": command,
+                "Args": args,
+                "Kwargs": kwargs,
+                "Traceback": tb_lines}
+
+        json.dump(data, logfile, indent=4)
 
 
 class Bot(commands.Bot):
@@ -24,7 +58,6 @@ class Bot(commands.Bot):
     async def on_ready(self):
         print("Name:\t{0}\nID:\t{1}".format(super().user.name, super().user.id))
 
-    # TODO Make this log errors with the command + args used
     async def on_command_error(self, ctx, exception):
         # This prevents any commands with local handlers being handled here in on_command_error.
         if hasattr(ctx.command, 'on_error'):
